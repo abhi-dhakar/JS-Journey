54838b048d5bf9b4c74ed761466ce91e8d5dfc81
abhi dhakar <nagarabhishek011@gmail.com>
2025-05-28 15:35:07 +0530

    Add control flow examples: if-else, nested if, and switch statements

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -13,3 +13,23 @@ switch (day) {
     default:
         console.log("It's just another day.");
 }
+
+
+
+/// Nullish coalescing Operator
+
+let val1;
+//val1 = 10 ?? 20
+//console.log(val1); // 10
+
+//val1 = null ?? 10
+//console.log(val1); // 10
+//val1 = undefined ?? 10
+//console.log(val1); //10
+
+val1 = null ?? undefined
+console.log(val1); // undegined
+
+
+
+
